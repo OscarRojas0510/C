1c2b566e3617a4972cec64b585ba0f8403d04593
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-11 16:52:45 -0600

    Creación de proyecto y clase "Tipos de variable básicas"

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>//Libreria STandarD Input/Output
-/*
-Comentario de varias lineas
-*/
-int main()
-{
-    printf("Hola mundo");
-    return 0;
-}
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int varEntero = 1;
+    float varFloat = 1.5123456789012f;
+    double varDouble = 1.5123456789012;
+    char c = 'a';
+
+    printf("VarEntero = %d\nVarFloat = %f\nVarDouble = %f\nvarChar= %c", varEntero, varFloat, varDouble, c);
+
+    return 0;
+}
