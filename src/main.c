05c6f27d9642f21857b578239546f7578dd0f9bf
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-22 21:49:05 -0600

    Ejercicio de lecturas

--- src/main.c
+++ src/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    /*
-    Pedir info desde consola
-    */
-    int cadena_length = 100;
-    char cadena[cadena_length];
-    printf("Ingresa un nombre: \n");
+    char nombre[100];
+    int edad;
+    double sueldo;
+    bool esEmpConf;
+    printf("Proporciona tu nombre: \n");
+    scanf("%[^\n]s", nombre);
+    printf("Proporciona tu edad : \n");
+    scanf("%d", &edad);
+    printf("Proporciona tu sueldo (USD)\n");
+    scanf("%lf", &sueldo);
+    printf("Eres empleado de confianza? (1/0)\n");
+    scanf("%d", &esEmpConf);
 
-    gets(cadena);
-
-    printf("\nEl nombre ingresado fue: %s\n", cadena);
-
+    printf("La información ingresada es:\n");
+    printf("Nombre: %s\n", nombre);
+    printf("Edad: %i\n", edad);
+    printf("Sueldo: $%0.2lf\n", sueldo);
+    printf("Empleado de confianza: %d\n", esEmpConf);
     return 0;
 }
