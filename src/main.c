15315357783dcce1d6c20cd5d1de19f3886ffe41
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-22 20:01:43 -0600

    Leer cadenas con espacios

--- src/main.c
+++ src/main.c
@@ -5,11 +5,11 @@ int main()
     /*
     Pedir info desde consola
     */
-    int cadena_length = 10;
+    int cadena_length = 100;
     char cadena[cadena_length];
     printf("Ingresa un nombre: \n");
 
-    scanf("%s", cadena);
+    scanf("%[^\n]", cadena);
     printf("\nEl nombre ingresado fue: %s\n", cadena);
 
     return 0;
