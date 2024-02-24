537bccb1fa1d124a61c983a1998bee7da0f88e7f
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-23 22:32:48 -0600

    obtener tamaño en bytes de las variables en C

--- src/main.c
+++ src/main.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
-// Otra forma de usar constantes.
-#define PI 3.1416
-// Usar constante PI de la libreria math
-#include <math.h>
+#include <stdbool.h>
 
 int main()
 {
+    // sizeof
     /*
-    Constantes en C:
-
-    Sintaxis:
-
-    const <tipo> <nombre> = valor;
-
-    A diferencia de una variable, la constante de debe inicializar
-    en el momento de declararla.
-
-    Una constante es inmutable, por lo que no se puede cambiar su valor
-    en el tiempo de ejecución, solo en el tiempo de compilación.
+    Sirve para ver el tamaño de las variables em bytes
     */
+    int entero;
+    float flotante;
+    double doble;
+    char caracter;
+    char cadena[] = "Hola mundo";
+    bool boleano = false;
+    long largo;
 
-     // como regla, no se usa camelCase, si no se usa snake_case
-    // MI_CONSTANTE =20;  //esto arroja un error
-    printf("Este es el valor de la constante: %d\n", MI_CONSTANTE);
-    printf("Este es el valor de la constante al inicio del archivo: %.4f\n", PI);
-    printf("Este es el valor de la constante PI de la libreria math: %.50lf\n", M_PI);
+    printf("tamaño en bytes de un entero: %lu\n", sizeof(entero));     // 4
+    printf("tamaño en bytes de un float: %lu\n", sizeof(flotante));    // 4
+    printf("tamaño en bytes de un double: %lu\n", sizeof(doble));      // 8
+    printf("tamaño en bytes de un caracter: %lu\n", sizeof(caracter)); // 1
+    printf("tamaño en bytes de una cadena : %lu\n", sizeof(cadena));   // 11
+    printf("tamaño en bytes de un booleano: %lu\n", sizeof(boleano));  // 1
+    printf("tamaño en bytes de un long: %lu\n", sizeof(largo));        // 8
 
-    /*
-    
-    */
     return 0;
 }
