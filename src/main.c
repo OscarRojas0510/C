ae197122c9bdfe336b7029eec5231a2a806920a0
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-23 22:39:46 -0600

    arrays en C

--- src/main.c
+++ src/main.c
@@ -3,25 +3,14 @@
 
 int main()
 {
-    // sizeof
     /*
-    Sirve para ver el tamaño de las variables em bytes
+    Arreglos
     */
-    int entero;
-    float flotante;
-    double doble;
-    char caracter;
-    char cadena[] = "Hola mundo";
-    bool boleano = false;
-    long largo;
-
-    printf("tamaño en bytes de un entero: %lu\n", sizeof(entero));     // 4
-    printf("tamaño en bytes de un float: %lu\n", sizeof(flotante));    // 4
-    printf("tamaño en bytes de un double: %lu\n", sizeof(doble));      // 8
-    printf("tamaño en bytes de un caracter: %lu\n", sizeof(caracter)); // 1
-    printf("tamaño en bytes de una cadena : %lu\n", sizeof(cadena));   // 11
-    printf("tamaño en bytes de un booleano: %lu\n", sizeof(boleano));  // 1
-    printf("tamaño en bytes de un long: %lu\n", sizeof(largo));        // 8
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    for (int i = 0; i < (int)sizeof(arr) / 4; i++)
+    {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
 
     return 0;
 }
