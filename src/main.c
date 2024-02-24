7e5f7fd98d138f11fac595e1b66625bb89d2c094
OscarRojas0510 <oscarrojasarriagax3@gmail.com>
2024-02-23 19:21:12 -0600

    Definición de constantes en C

--- src/main.c
+++ src/main.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
-#include <stdbool.h>
+// Otra forma de usar constantes.
+#define PI 3.1416
+// Usar constante PI de la libreria math
+#include <math.h>
 
 int main()
 {
-    char nombre[100];
-    int edad;
-    double sueldo;
-    bool esEmpConf;
-    int esEmpConfTemp;
-    printf("Proporciona tu nombre: \n");
-    scanf("%[^\n]s", nombre);
-    printf("Proporciona tu edad : \n");
-    scanf("%d", &edad);
-    printf("Proporciona tu sueldo (USD)\n");
-    scanf("%lf", &sueldo);
-    printf("Eres empleado de confianza? (1/0)\n");
-    scanf("%d", &esEmpConfTemp);
-    esEmpConf = esEmpConfTemp;
+    /*
+    Constantes en C:
 
-    printf("La información ingresada es:\n");
-    printf("Nombre: %s\n", nombre);
-    printf("Edad: %i\n", edad);
-    printf("Sueldo: $%0.2lf\n", sueldo);
-    printf("Empleado de confianza: %d\n", esEmpConf);
+    Sintaxis:
+
+    const <tipo> <nombre> = valor;
+
+    A diferencia de una variable, la constante de debe inicializar
+    en el momento de declararla.
+
+    Una constante es inmutable, por lo que no se puede cambiar su valor
+    en el tiempo de ejecución, solo en el tiempo de compilación.
+    */
+
+     // como regla, no se usa camelCase, si no se usa snake_case
+    // MI_CONSTANTE =20;  //esto arroja un error
+    printf("Este es el valor de la constante: %d\n", MI_CONSTANTE);
+    printf("Este es el valor de la constante al inicio del archivo: %.4f\n", PI);
+    printf("Este es el valor de la constante PI de la libreria math: %.50lf\n", M_PI);
+
+    /*
+    
+    */
     return 0;
 }
